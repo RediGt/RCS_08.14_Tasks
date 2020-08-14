0e736df9234baf0ed18ee1d8779824568667d613
RediGt <red.i@inbox.lv>
2020-08-14 16:51:07 +0300

    Add project files.

---
+++ RCS_Tasks/FileIO.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace RCS_Tasks
+{
+    class FileIO
+    {
+        public static List<int> Read()
+        {
+            String line;
+            List<int> a = new List<int>();
+            try
+            {
+                StreamReader sr = new StreamReader("Test.txt");
+
+                line = sr.ReadLine();
+
+                while (line != null)
+                {
+                    a.Add(Convert.ToInt32(line));
+                    line = sr.ReadLine();
+                }
+                sr.Close();
+                return a;
+            }
+            catch
+            {
+                Console.WriteLine("Neizdevas atvert failu!");
+                return null;
+            }
+        }
+
+        public static void Write(List<int> a)
+        {
+            try
+            {
+                StreamWriter sw = new StreamWriter("Test.txt", false);
+
+                foreach (var item in a)                  
+                    sw.WriteLine(item);
+
+                sw.Close();
+            }
+            catch
+            {
+                Console.WriteLine("Neizdevas ierakstit faila!");
+            }
+        }
+    }
+}
---
+++ RCS_Tasks/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace RCS_Tasks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Task1();
+        }
+
+        static void Task1()
+        {
+            List<int> a = new List<int>();
+            Console.WriteLine("Randomized list:");
+            RandomizeList(a);
+            PrintList(a);
+            Console.WriteLine();
+
+            FileIO.Write(a);
+            List<int> b = FileIO.Read();
+            if( b != null)
+            {
+                Console.WriteLine("Readed list:");
+                PrintList(b);
+            }
+               
+        }
+
+        static void RandomizeList(List<int> a)
+        {
+            Random rand = new Random();
+            for (int i = 0; i < 10; i++)
+            {
+                a.Add(rand.Next(0, 11));
+            }
+        }
+
+        static void PrintList(List<int> a)
+        {
+            foreach (var item in a)
+                Console.Write(item + "  ");
+        }
+    }
+}
